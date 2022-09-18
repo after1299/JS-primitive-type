71a271082a46900c901a6e3aa935af8c89071f0d
after1299 <113080959+after1299@users.noreply.github.com>
2022-09-18 14:56:47 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+// JavaScipt's primitive data type
+
+// number
+// toString()
+let age = 10;
+let ageStr = age.toString();
+console.log(ageStr + 20);
+
+// toFix()
+const pi = 3.14159
+const piFix = pi.toFixed(2);
+console.log(piFix);
